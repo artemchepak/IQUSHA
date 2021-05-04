4ea53a4283464913e5a6fc60757eca9d27500d83
Artem Chepak <artemchepakwebdeveloper@users.noreply.github.com>
2021-05-04 18:41:39 +0300

    feat: add all sections

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,20 @@
-$(function () {
+$(function() {
+    $('.games__item-slider').slick({
+        arrows: true,
+        prevArrow: "<img class='slick-prev slick-btn' src='../images/icons/previous.svg'>",
+        nextArrow: "<img class='slick-next slick-btn' src='images/icons/next.svg'>"
+    });
 
+    $('.reviews__items').slick({
+        arrows: false,
+        infinite: true,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        dots: true
+    });
+
+    $('.faq__list-title').on('click', function(e) {
+        jQuery(this).toggleClass('faq__list-title--active');
+        jQuery(this).next().slideToggle('200');
+    });
 })
